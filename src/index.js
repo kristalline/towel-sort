862883c9b2bf17c5efff83d9fa898df6776daf69
evoly <svetoch.svs@gmail.com>
2024-08-12 19:57:41 +0700

    add :  function towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
+const isEven = (num) => num % 2 === 0;
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+
+  return matrix.reduce( (acc, el, index) => {
+    let temp = !isEven(index) ? el.reverse() : el
+    acc.push(...temp);
+    return acc;
+  }, []);
 }
